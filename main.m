eeb0e49bbb44f493432677ef518a244cf462841c
EliottTotman <111763807+EliottTotman@users.noreply.github.com>
2025-01-30 09:44:36 -0700

    Update main.m

--- main.m
+++ main.m
@@ -2,3 +2,9 @@ close all; clear; clc;
 
 % FUNCTION RotationMatrix321 - Intakes [alpha, beta, gamma] and outputs rotation DCM for Inertial->Body
 % FUNCTION RotationMatrix313 - Intakes [alpha, gamma, alpha] and outputs rotation DCM for Inertial->Body
+
+% FUNCTION EulerAngles321 - Intakes 3x3 [DCM] and outputs 3 Euler Angles
+% for 321 orientation
+% FUNCTION EulerAngles313 - Intakes 3x3 [DCM] and outputs 3 Euler Angles
+% for 313 orientation
+
