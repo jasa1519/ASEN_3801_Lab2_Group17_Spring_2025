4f56c6d68a83cada9ae140d3412fad150c436ad3
jasa1519 <156624459+jasa1519@users.noreply.github.com>
2025-02-06 09:11:24 -0700

    Update EulerAngles313.m

--- EulerAngles313.m
+++ EulerAngles313.m
@@ -2,7 +2,7 @@ function attitude313 = EulerAngles313(DCM)
 %ATTITUDE313 Extracts attitude (in 3-1-3) from a DCM
 %   Detailed explanation goes here
 
-alpha = atan2(DCM(3,1),-1*DCM(3,3));
+alpha = atan2(DCM(3,1),-1*DCM(3,2));
 beta = acos(DCM(3,3));
 gamma = atan2(DCM(1,3),DCM(2,3));
 
@@ -10,4 +10,3 @@ attitude313 = [alpha;
                beta;
                gamma];
 end
-
