c24e99bb8d7e32f4d153d9904bfb1aa715acc5fb
jasa1519 <156624459+jasa1519@users.noreply.github.com>
2025-02-13 10:39:23 -0700

    Update main.m
    Updated through task 7

--- main.m
+++ main.m
@@ -39,18 +39,19 @@ figure()
 hold on
 plot3(target.position_N(1,:),target.position_N(2,:),target.position_N(3,:))
 plot3(vehicle.position_N(1,:),vehicle.position_N(2,:),vehicle.position_N(3,:))
-
+title("Task 3")
 hold off
 
 
 %% Task 4
 
 
-% Axis Positions (Frame E)
+% Axis Positions (Pos)
 subplot_label_arr = ["x","y","z"];
 figure()
+sgtitle("Task 4 (Pos)")
 for i = 1:3
-subplot(2,2,i)
+subplot(3,1,i)
 hold on
 plot(t_vec,target.position_E(i,:),"Color",[0,0,1])
 plot(t_vec,vehicle.position_E(i,:),"Color",[1,0,0])
@@ -61,8 +62,9 @@ end
 % Euler 3-2-1 Rel to E
 subplot_label_arr = ["\phi","\theta","\psi"];
 figure()
+sgtitle("Task 4 (3-2-1)")
 for i = 1:3
-subplot(2,2,i)
+subplot(3,1,i)
 hold on
 plot(t_vec,(180/pi).*target.attitude_E(i,:),"Color",[0,0,1])
 plot(t_vec,(180/pi).*vehicle.attitude_E(i,:),"Color",[1,0,0])
@@ -82,9 +84,9 @@ vehicle.attitude_E313 = zeros(3,length(t_vec));
 % Loop takes attitude at each point, gets the DCM, and then extracts 313
 % Euler angle.
 for i = 1:length(t_vec)
-vehicle_DCM = RotationMatrix321(vehicle.attitude_E(:,i));
+vehicle_DCM = RotationMatrix321((180/pi).*vehicle.attitude_E(:,i));
 vehicle.attitude_E313(:,i) = EulerAngles313(vehicle_DCM);
-target_DCM = RotationMatrix321(target.attitude_E(:,i));
+target_DCM = RotationMatrix321((180/pi).*target.attitude_E(:,i));
 target.attitude_E313(:,i) = EulerAngles313(target_DCM);
 end
 
@@ -93,8 +95,9 @@ end
 % Euler 3-1-3 Rel to E
 subplot_label_arr = ["\phi","\theta","\psi"];
 figure()
+sgtitle("Task 5 (3-1-3)")
 for i = 1:3
-subplot(2,2,i)
+subplot(3,1,i)
 hold on
 plot(t_vec,(180/pi).*target.attitude_E313(i,:),"Color",[0,0,1])
 plot(t_vec,(180/pi).*vehicle.attitude_E313(i,:),"Color",[1,0,0])
@@ -103,5 +106,52 @@ title(subplot_label_arr(i))
 hold off
 end
 
+%% Task 6
 
+% Plotting components of relative position vector.
+
+relative_position_E = target.position_E-vehicle.position_E;
+
+subplot_label_arr = ["x","y","z"];
+figure()
+sgtitle("Task 6 (Relative in Earth)")
+for i = 1:3
+subplot(3,1,i)
+hold on
+plot(t_vec,relative_position_E(i,:))
+title(subplot_label_arr(i))
+hold off
+end
+
+%% Task 7
+
+% Plot relative position vector in vehicle coords.
+
+relative_position_B = zeros(3,length(t_vec));
+
+for i = 1:length(t_vec)
+    vehicle_DCM = RotationMatrix321((180/pi).*vehicle.attitude_E(:,i));
+    relative_position_B(:,i) = (vehicle_DCM*target.position_E(:,i)-vehicle_DCM*vehicle.position_E(:,i));
+    if(0 == mod(i,10))
+        teststop = 0;
+    end
+end
+
+testDCM = RotationMatrix321([30,60,120]);
+
+subplot_label_arr = ["x","y","z"];
+figure()
+sgtitle("Task 7 (Relative in Vehicle)")
+for i = 1:3
+subplot(3,1,i)
+hold on
+plot(t_vec,relative_position_B(i,:))
+title(subplot_label_arr(i))
+hold off
+
+end
+
+
+
+%% Task 8
 
